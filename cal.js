170246085f2c4f046561155ac6165f4fe82be2b4
MUHAMMAD UBAID BIN RASHID <ubaidqadri97@gmail.com>
2024-05-06 15:23:15 -0700

    ok

--- cal.js
+++ cal.js
@@ -23,13 +23,17 @@ let body = document.querySelector('.kuch-bhi')
 let main = document.querySelector('.main-two')
 let iF = document.querySelector('.if')
 let eLse = document.querySelector('.else')
-
+let pehLay = document.querySelector('.pehlay')
 function forDark(){
     body.classList.remove('body')
     eLse.classList.remove('none')
     iF.classList.add('none')
+    pehLay.classList.add('white')
+    pehLay.classList.remove('black')
 }
 function forLight(){
+    pehLay.classList.remove('white')
+    pehLay.classList.add('black')
     body.classList.add('body')
     eLse.classList.add('none')
     iF.classList.remove('none')
